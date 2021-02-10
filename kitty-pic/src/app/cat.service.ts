6c4734df2eb96113e9613f8bd288f4d0f26485d6
deliaturner <deliajturner@gmail.com>
2021-02-09 19:47:55 -0500

    API Service

---
+++ kitty-pic/src/app/cat.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatService {
+
+  // apiKey: string = '341b3a8d-6eb2-4eaa-809f-b5fb4245d695';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCatPic(): any {
+    return this.httpClient.get(`https://api.thecatapi.com/images/search?mime_types=jpg,png`);
+  }
+
+  getCatGif(): any {
+    return this.httpClient.get(`https://api.thecatapi.com/images/search?mime_types=gif`);
+  }
+
+}
